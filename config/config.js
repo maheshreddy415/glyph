84d8c7b7abbf71cd24b2cb7d721c86eba81485c2
Mahesh Kumar Parlapalli <mahesh.parlapalli@cognizant.com>
2016-09-06 23:13:43 +0530

    Create config.js

---
+++ config/config.js
@@ -0,0 +1,5 @@
+{
+    "connectionString": "mongodb://localhost:27017/glyph",
+    "secret": "x9b2g6",
+    "userCollectionFields": ["name", "emp_id", "project_id", "project_name", "project_description", "date_request_made", "email_id", "action_taken", "date_responded"]
+}
